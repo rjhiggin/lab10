809b38409df166d05e193d313513e221cffbb111
Ryan <rjhiggin@umich.edu>
2020-04-09 15:48:53 -0400

    commit for lab 10

--- SelectionSort.java
+++ SelectionSort.java
@@ -5,14 +5,14 @@ public class SelectionSort {
 	/** Creates a new instance of SelectionSort */
    public SelectionSort() {
    }
-   
+
    /* A simple SelectionSort algorithm
-    * pre-condition: 
+    * pre-condition:
     * post-condition:
     * inputs:
     * outputs:
     * special conditions:
-    */    
+    */
    public int[] basicSelectionSort(int[] x) {
        for (int i = 0; i < x.length; i++) {
            for (int j= i+1; j < x.length; j++) {
@@ -25,5 +25,5 @@ public class SelectionSort {
        } // end of outer for loop
        return x;
    } // end of basicSelectionSort method
-
+//
 }
--- testSelectionSort.java
+++ testSelectionSort.java
@@ -14,7 +14,7 @@ class testSelectionSort {
 		testDuplicates();
 	}
 
-    
+
     public void testPositive(){
 	//Branch bob change
         int[] arr = new int[5];
@@ -23,25 +23,25 @@ class testSelectionSort {
         arr[2] = 7;
         arr[3] = 10;
         arr[4] = 2;
-        
+
         int[] Sortedarr = new int[5];
         Sortedarr[0] = 2;
         Sortedarr[1] = 7;
         Sortedarr[2] = 8;
         Sortedarr[3] = 9;
         Sortedarr[4] = 10;
-        
+
         SelectionSort ss = new SelectionSort();
         arr = ss.basicSelectionSort(arr);
-        
+
         /** add tests to check for this unit test **/
-        
+
         assertEquals("first valuses are not equal",Sortedarr[0], arr[0]);
         assertEquals("second value is not equal to 7", arr[1], 7);
         assertTrue("sorted array values are not sorted", arr[0] < arr[1]);
         assertArrayEquals("arrays are not eqaul", arr, Sortedarr);
     }
-    
+
     public void testNegative(){
 	//branch bob change
     	int[] arr = new int[5];
@@ -50,28 +50,28 @@ class testSelectionSort {
         arr[2] = -7;
         arr[3] = -10;
         arr[4] = -2;
-        
+
         SelectionSort ss = new SelectionSort();
-        
+
         int[]Sortedarr = new int[5];
         Sortedarr[0] = -10;
         Sortedarr[1] = -9;
         Sortedarr[2] = -8;
         Sortedarr[3] = -7;
         Sortedarr[4] = -2;
-        
+
         arr = ss.basicSelectionSort(arr);
-        
-        
+
+
         /** Test data contains negative values only **/
         assertEquals("first valuses are not equal",Sortedarr[0], arr[0]);
         assertTrue("sorted array values are not sorted", arr[0] < arr[1]);
         assertTrue("Last value is not -2", arr[4] == -2);
         assertArrayEquals("arrays are not eqaul", arr, Sortedarr);
-    
-        
+
+
     }
-    
+
     public void testMixed(){
 	// Tom changes
     	int[] arr = new int[5];
@@ -80,27 +80,27 @@ class testSelectionSort {
         arr[2] = -7;
         arr[3] = -10;
         arr[4] = 2;
-        
+
         SelectionSort ss = new SelectionSort();
-        
+
         int[]Sortedarr = new int[5];
         Sortedarr[0] = -10;
         Sortedarr[1] = -8;
         Sortedarr[2] = -7;
         Sortedarr[3] = 0;
         Sortedarr[4] = 2;
-        
+
         arr = ss.basicSelectionSort(arr);
-        
+
         /** Test data contains with both positive, negative and zeros **/
         assertEquals("first valuses are not equal",Sortedarr[0], arr[0]);
         assertTrue("sorted array values are not sorted", arr[0] < arr[1]);
         assertTrue("Last value is not 2", arr[4] == 2);
         assertArrayEquals("arrays are not eqaul", arr, Sortedarr);
-        
-    	
+
+
     }
-    
+
     public void testDuplicates(){
 	//Tom changes
     	int[] arr = new int[5];
@@ -109,25 +109,25 @@ class testSelectionSort {
         arr[2] = 7;
         arr[3] = 10;
         arr[4] = 2;
-        
+
         SelectionSort ss = new SelectionSort();
-        
+
         int[]Sortedarr = new int[5];
         Sortedarr[0] = 2;
         Sortedarr[1] = 2;
         Sortedarr[2] = 7;
         Sortedarr[3] = 7;
         Sortedarr[4] = 10;
-        
+
         arr = ss.basicSelectionSort(arr);
-        
+
         /** Test data contains duplicates **/
         assertEquals("first valuses are not equal",Sortedarr[0], arr[0]);
         assertTrue("sorted array values are not the same", arr[0] == arr[1]);
         assertTrue("Last value is not 10", arr[4] == 10);
         assertArrayEquals("arrays are not eqaul", arr, Sortedarr);
-        
-       
+
+
     }
 
 }
